3eff93eee780e499a4530bc23529af52027ba754
Al-abeid Abdou Soimadou <111879708+abeidabdou@users.noreply.github.com>
2024-06-20 17:57:04 +0300

    Rectifying error in deletemessage for members when it comes to category channels second try

--- command/deleteMessage.py
+++ command/deleteMessage.py
@@ -258,7 +258,7 @@ async def deleteMessage(ctx, command):
                 responses.append(
                     f"{language['delete_message_category_and_forum_no']}: {channel.name} {language['is']} {channel.type}")
 
-            if ("wal-i" in channel.name or ctx.user not in channel.members) and channel.type not in (discord.ChannelType.category):
+            if channel.type not in (discord.ChannelType.category) and ("wal-i" in channel.name or ctx.user not in channel.members) :
                 channels.remove(channel)
                 responses.append(
                     f"{language['delete_message_in_channel_no_permission']} : {channel.name}")
